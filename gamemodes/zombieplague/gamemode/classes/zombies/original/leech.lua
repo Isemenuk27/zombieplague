25042daf932a3a47ff737a40573c28db5234e118
Erick Maksimets <59126348+ErickMaksimets@users.noreply.github.com>
2022-05-14 19:58:14 +0300

    Add files via upload

--- gamemodes/zombieplague/gamemode/classes/zombies/original/leech.lua
+++ gamemodes/zombieplague/gamemode/classes/zombies/original/leech.lua
@@ -1,9 +1,9 @@
 ZPClass.Name = "ZombieLeechClassName"
 ZPClass.Description = "ZombieLeechClassDescription"
-ZPClass.MaxHealth = 1000
+ZPClass.MaxHealth = 1500
 ZPClass.PModel = "models/player/soldier_stripped.mdl"
-ZPClass.Speed = 260
-ZPClass.RunSpeed = 280
+ZPClass.Speed = 280
+ZPClass.RunSpeed = 100
 ZPClass.CrouchSpeed = 0.5
 ZPClass.Gravity = 0.7
 ZPClass.InfectionFunction = function(Attacker, Infected)
