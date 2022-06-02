c0f5a56bb8529bf8740feb9ff32ec711b2e564c8
Isemenuk27 <77487254+Isemenuk27@users.noreply.github.com>
2022-06-02 13:13:15 +0300

    Add files via upload

--- gamemodes/zombieplague/gamemode/weapons/hl2/hl2_crossbow.lua
+++ gamemodes/zombieplague/gamemode/weapons/hl2/hl2_crossbow.lua
@@ -3,3 +3,7 @@ Weapon.WeaponID = "weapon_crossbow"
 Weapon.DamageMultiplier = 2.2
 Weapon.WeaponType = WEAPON_PRIMARY
 Weapon.ProjectileID = "crossbow_bolt"
+function Weapon:GiveWeapon(ply)
+    local Weap = ply:Give(self.WeaponID)
+    ply:GiveAmmo(50, Weap:GetPrimaryAmmoType(), true)
+end
